1e0bba72f9d64d2339ed4e0907db047542711f8c
linoxolus <linoxolus@gmail.com>
2023-08-02 21:02:18 +0700

    Final: raw data WebServer

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const port = 2008; 
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname ,'resources', 'views'));
+
 app.get('/', (req, res) => {
-    res.send('test');
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
 })
 
 app.listen(port, () => {
-    console.log(`Open WebServer Success at: http://localhost:${port}`)
-})
+    console.clear();
+    console.log(`Open WebServer Success at: http://localhost:${port}`);
+});
