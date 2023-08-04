9c34fc222df789736aaeece0c46358ddd0a03b81
linoxolus <linoxolus@gmail.com>
2023-08-04 15:39:19 +0700

    Add: static file and comment

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,10 +1,17 @@
+// Import liblary
+const exp = require('constants');
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 
+// Declaration variable
 const app = express();
 const port = 2008; 
 
+// Config static file
+app.use(express.static(path.join(__dirname, 'resources', 'public')))
+
+// Config view engine
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname ,'resources', 'views'));
